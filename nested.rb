3c748f561a58e58515e3bf62e0a55fcc4b27f356
Zac Steinberg <zstein1234@gmail.com>
2019-08-13 22:18:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,14 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+  }
+  
+  puts programmer_hash
+      
 end
 
 def changing_alan
