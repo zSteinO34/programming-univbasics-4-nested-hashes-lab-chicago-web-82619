43af6bb7d15717650d6b8d49b6292922759ecb1e
Zac Steinberg <zstein1234@gmail.com>
2019-08-13 22:15:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-     return programmer_hash[:dennis_ritchie][:languages].to_s
+     return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
