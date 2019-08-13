a3f770d9f6505371d3f5273dc5f9e716256494fb
Zac Steinberg <zstein1234@gmail.com>
2019-08-13 22:24:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,10 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash[:yukihiro_matsumoto] = {
-       :known_for => "Ruby",
-       :languages => ["LISP", "C"]
-     }
+     programmer_hash[:yukihiro_matsumoto] = {}
     puts programmer_hash
 end
 
